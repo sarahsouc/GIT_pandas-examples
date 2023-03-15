8ef31ecea1fae4a8944745c6aa1de1cf76f8a186
alexis <alexis.favrefelix@le-campus-numerique.fr>
2023-03-15 12:45:45 +0100

    my version is better bitch

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -60,9 +60,7 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-
 Luffy = cloneDF(mergeRatings)
 Luffy= Sarah.groupby(['movie_id', 'title'])['rating'].agg(
-
     COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values('COUNT', ascending=False)
 print('My info sorted: \n%s' % sortRatingsField[:15])
