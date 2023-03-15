2b22ad8a33d4edd7ca9af7b87fb1727819e4ae77
Sarah Souchon <sarah.souchon@gmail.com>
2023-03-15 11:38:18 +0100

    Erreur corrigée, sort remplacé par sot-values

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -62,5 +62,5 @@ print('\n==================================================================\n')
 # Sort data ratings by created field (groupby + lambda function + sorted)
 sortRatingsField = cloneDF(mergeRatings)
 sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
-    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
+    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values('COUNT', ascending=False)
 print('My info sorted: \n%s' % sortRatingsField[:15])
